aecc60b57c82325899589f9843cd0bab77cded47
Pierce Edmiston <pedmiston@wisc.edu>
2014-03-21 21:47:08 -0500

    Added the second study to the two-studies example.
    Table displays correct values. CI plot is set, but calculations are filler.

--- two-studies/server.R
+++ two-studies/server.R
@@ -1,13 +1,30 @@
 library(shiny)
+library(ggplot2)
 
 shinyServer(function(input, output) {
+  calcNHST <- function (m, sd, n) {
+    df <- n + n - 2
+    t <- m / (sd * sqrt(1/n + 1/n))
+    p <- 2 * pt(-t, df = df)
+    return(data.frame('M.diff' = m,
+                      'SD.pooled' = sd,
+                      'df' = df,
+                      't' = t,
+                      'p' = sprintf('%1.4f', p)))
+  }
+  
   output$nhst <- renderTable({
-    t.obt <- -input$m1 / (input$sd1 * sqrt(1/input$n1 + (1/input$n1)))
-    p.obt <- 2 * pt(t.obt, df = input$n1 + input$n1 - 2)
-    results <- list('M(diff)' = input$m1,
-                    'SD(pooled)' = input$sd1,
-                    't value' = t.obt,
-                    'p value' = p.obt)
-    as.data.frame(results)
+    rbind(calcNHST(input$m1, input$sd1, input$n1),
+          calcNHST(input$m2, input$sd2, input$n2))
+  })
+  
+  output$ci <- renderPlot({
+    data <- rbind(calcNHST(input$m1, input$sd1, input$n1),
+                  calcNHST(input$m2, input$sd2, input$n2))
+    print(ggplot(data, aes(x = c(1,2), y = M.diff, ymin = M.diff - SD.pooled, ymax = M.diff + SD.pooled)) +
+      geom_point() +
+      geom_errorbar() + 
+      coord_flip(ylim = c(0, 10)) +
+      scale_x_reverse('Study', breaks = c(1,2)))
   })
 })
--- two-studies/ui.R
+++ two-studies/ui.R
@@ -1,19 +1,31 @@
 library(shiny)
 
+SLIDER_STEP_M <- 0.1
+SLIDER_STEP_SD <- 0.1
+
 shinyUI(pageWithSidebar(
 
   # Application title
   headerPanel("Two studies"),
 
   # Sidebar with a slider input for number of observations
-  sidebarPanel(
-    sliderInput("m1", "M(diff)", value = 4, min = 0, max = 10),
-    sliderInput("sd1", "SD(pooled)", value = 2.83, min = 0.01, max = 20),
-    numericInput("n1", "N", value = 20, min = 2, max = NA, step = 2)
+  sidebarPanel(    
+    sliderInput("m1", "M(diff)", value = 4, min = 0, max = 10, 
+                ticks = TRUE, step = SLIDER_STEP_M),
+    sliderInput("sd1", "SD(pooled)", value = 2.83, min = 0.01, max = 20, 
+                ticks = TRUE, step = SLIDER_STEP_SD),
+    numericInput("n1", "N", value = 20, min = 2, max = NA, step = 2),
+    br(),
+    sliderInput("m2", "M(diff)", value = 4, min = 0, max = 10, 
+                ticks = TRUE, step = SLIDER_STEP_M),
+    sliderInput("sd2", "SD(pooled)", value = 2.83, min = 0.01, max = 20, 
+                ticks = TRUE, step = SLIDER_STEP_SD),
+    numericInput("n2", "N", value = 20, min = 2, max = NA, step = 2)
   ),
 
   # Show a plot of the generated distribution
   mainPanel(
-    tableOutput("nhst")
+    tableOutput("nhst"),
+    plotOutput("ci")
   )
 ))
