99566cae1c3e4c94f4f646a72e740323de69b720
Pierce Edmiston <pedmiston@wisc.edu>
2014-03-22 15:51:40 -0500

    Updated CI stats for two-studies app.

--- two-studies/server.R
+++ two-studies/server.R
@@ -1,30 +1,60 @@
 library(shiny)
 library(ggplot2)
+library(grid)
+library(plyr)
 
 shinyServer(function(input, output) {
-  calcNHST <- function (m, sd, n) {
-    df <- n + n - 2
-    t <- m / (sd * sqrt(1/n + 1/n))
-    p <- 2 * pt(-t, df = df)
-    return(data.frame('M.diff' = m,
-                      'SD.pooled' = sd,
-                      'df' = df,
-                      't' = t,
-                      'p' = sprintf('%1.4f', p)))
+  calcStats <- function () {
+    results <- data.frame("study" = factor(c("Luck", "Noluck"), levels = c("Noluck", "Luck"), ordered = TRUE),
+                          "m.diff" = c(input$m1, input$m2),
+                          "sd.pooled" = c(input$sd1, input$sd2),
+                          "n" = c(input$n1, input$n2))
+    results$df <- with(results, n + n - 2)
+    results$t.95 <- qt(0.975, df = results$df)
+    results$t.obt <- with(results, m.diff / (sd.pooled * sqrt(2/n)))
+    results$p.obt <- 2 * pt(-results$t.obt, df = results$df)
+    results$se.diff <- with(results, sqrt(((sd.pooled^2) * 2) / n))
+    results$ci.lwr <- with(results, m.diff - (t.95 * se.diff))
+    results$ci.upr <- with(results, m.diff + (t.95 * se.diff))
+    
+    return(results)
   }
   
   output$nhst <- renderTable({
-    rbind(calcNHST(input$m1, input$sd1, input$n1),
-          calcNHST(input$m2, input$sd2, input$n2))
+    nhst <- calcStats() # [ , c("study","m.diff","sd.pooled","sd.diff","n","t.obt","p.obt")]
+    nhst$outcome <- ifelse(nhst$p.obt <= 0.05, "significant", "nonsignificant")
+    nhst$p.obt <- sprintf("%1.4f", nhst$p.obt)
+#     nhst <- rename(nhst, c("study" = "Study",
+#                            "m.diff" = "M.diff",
+#                            "sd.pooled" = "SD.pooled",
+#                            "sd.diff" = "SD.diff",
+#                            "n" = "N",
+#                            "t.obt" = "t.value",
+#                            "p.obt" = "p.value",
+#                            "outcome" = "Outcome"))
+    
+    return(nhst)
   })
   
   output$ci <- renderPlot({
-    data <- rbind(calcNHST(input$m1, input$sd1, input$n1),
-                  calcNHST(input$m2, input$sd2, input$n2))
-    print(ggplot(data, aes(x = c(1,2), y = M.diff, ymin = M.diff - SD.pooled, ymax = M.diff + SD.pooled)) +
+    est <- calcStats()
+    est.plot <- ggplot(est, aes(x = study, y = m.diff, ymin = ci.lwr, ymax = ci.upr)) +
       geom_point() +
-      geom_errorbar() + 
-      coord_flip(ylim = c(0, 10)) +
-      scale_x_reverse('Study', breaks = c(1,2)))
+      geom_errorbar(width = 0.2) + 
+      geom_hline(yintercept = 0.0, lty = 2) +
+      coord_flip(ylim = c(-2, 8)) +
+      scale_x_discrete("") +
+      scale_y_continuous("Mean (Diff)", breaks = seq(-2, 8, by = 1)) +
+      theme_bw()
+    print(est.plot)
   })
 })
+
+
+# theme(panel.background = element_blank(),
+#       axis.text = element_text(color = "black", size = 14),
+#       axis.line = element_line(size = 0.8),
+#       axis.line.y = element_blank(),
+#       axis.ticks.x = element_line(size = 0.8, color = "black"),
+#       axis.ticks.y = element_blank(),
+#       axis.ticks.length = unit(0.8, "lines"))
--- two-studies/ui.R
+++ two-studies/ui.R
@@ -1,31 +1,34 @@
 library(shiny)
 
 SLIDER_STEP_M <- 0.1
-SLIDER_STEP_SD <- 0.1
+SLIDER_STEP_SD <- 0.01
 
 shinyUI(pageWithSidebar(
-
-  # Application title
+  
   headerPanel("Two studies"),
-
-  # Sidebar with a slider input for number of observations
-  sidebarPanel(    
-    sliderInput("m1", "M(diff)", value = 4, min = 0, max = 10, 
+  
+  sidebarPanel(
+    h3("Study 1: Luck"),
+    sliderInput("m1", "M(diff)", value = 4.0, min = 0.0, max = 10.0, 
                 ticks = TRUE, step = SLIDER_STEP_M),
-    sliderInput("sd1", "SD(pooled)", value = 2.83, min = 0.01, max = 20, 
+    sliderInput("sd1", "SD(pooled)", value = 2.83, min = 0.1, max = 20.0, 
                 ticks = TRUE, step = SLIDER_STEP_SD),
     numericInput("n1", "N", value = 20, min = 2, max = NA, step = 2),
     br(),
-    sliderInput("m2", "M(diff)", value = 4, min = 0, max = 10, 
+    h3("Study 2: Noluck"),
+    sliderInput("m2", "M(diff)", value = 1.5, min = 0.0, max = 10.0, 
                 ticks = TRUE, step = SLIDER_STEP_M),
-    sliderInput("sd2", "SD(pooled)", value = 2.83, min = 0.01, max = 20, 
+    sliderInput("sd2", "SD(pooled)", value = 4.25, min = 0.1, max = 20.0, 
                 ticks = TRUE, step = SLIDER_STEP_SD),
-    numericInput("n2", "N", value = 20, min = 2, max = NA, step = 2)
+    numericInput("n2", "N", value = 30, min = 2, max = NA, step = 2)
   ),
 
   # Show a plot of the generated distribution
   mainPanel(
+    h3("NHST"),
     tableOutput("nhst"),
+    br(),
+    h3("95% CI"),
     plotOutput("ci")
   )
 ))
